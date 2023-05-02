15928d275a2c4e21e3a096481e7870c13fde16b2
Harsh-Yadav-02 <75542099+Harsh-Yadav-02@users.noreply.github.com>
2023-05-03 00:09:33 +0530

    update in selective repeat

--- selectiverepeat.cpp
+++ selectiverepeat.cpp
@@ -2,14 +2,14 @@
   Selective repeat sliding window protocol
 */
 
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-#include<cmath>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
 using namespace std;
 
 #define TOT_FRAMES 500
-#define FRAMES_SEND 10
+#define FRAMES_SEND 20
 
 class sel_repeat
 {
@@ -20,240 +20,224 @@ private:
   int rcvd[FRAMES_SEND];
   char rcvd_ack[FRAMES_SEND];
   int sw;
-  int rw;       //tells expected frame
+  int rw; // tells expected frame
 
 public:
-
   void input();
   void sender(int);
   void receiver(int);
-
 };
 
 void sel_repeat::input()
 {
 
-    int n;     //no. of bits for the frame
-    int m;    //no. of frames from n bits
-    int i;
-    cout<<"\nEnter the no. of bits for the sequence no. : ";
-    cin>>n;
-    m=pow(2,n);
+  int n; // no. of bits for the frame
+  int m; // no. of frames from n bits
+  int i;
+  cout << "\nEnter the no. of bits for the sequence no. : ";
+  cin >> n;
+  m = pow(2, n);
 
-    int t=0;
+  int t = 0;
 
-fr_send_at_instance=(m/2);
+  fr_send_at_instance = (m / 2);
 
-for(i=0;i<TOT_FRAMES;i++)
+  for (i = 0; i < TOT_FRAMES; i++)
 
-{
+  {
 
-arr[i]=t;
+    arr[i] = t;
 
-t=(t+1)%m;
+    t = (t + 1) % m;
+  }
 
-}
+  for (i = 0; i < fr_send_at_instance; i++)
 
-for(i=0;i<fr_send_at_instance;i++)
+  {
 
-{
+    send[i] = arr[i];
 
-send[i]=arr[i];
+    rcvd[i] = arr[i];
 
-rcvd[i]=arr[i];
+    rcvd_ack[i] = 'n';
+  }
 
-rcvd_ack[i]='n';
+  rw = sw = fr_send_at_instance;
 
-}
-
-rw=sw=fr_send_at_instance;
-
-sender(m);
-
+  sender(m);
 }
 
 void sel_repeat::sender(int m)
 
 {
 
-for(int i=0;i<fr_send_at_instance;i++)
+  for (int i = 0; i < fr_send_at_instance; i++)
 
-{
+  {
 
-if(rcvd_ack[i]=='n')
+    if (rcvd_ack[i] == 'n')
 
-cout<<"\nSENDER : Frame "<<send[i]<<" is sent\n";
+      cout << "\nSENDER : Frame " << send[i] << " is sent\n";
+  }
 
-}
-
-receiver(m);
-
+  receiver(m);
 }
 
 void sel_repeat::receiver(int m)
 
 {
 
-time_t t;
+  time_t t;
 
-int f;
-int j;
-int f1;
+  int f;
+  int j;
+  int f1;
 
-int a1;
+  int a1;
 
-char ch;
+  char ch;
 
-srand((unsigned)time(&t));
+  srand((unsigned)time(&t));
 
-for(int i=0;i<fr_send_at_instance;i++)
+  for (int i = 0; i < fr_send_at_instance; i++)
 
-{
-
-if(rcvd_ack[i]=='n')
-
-{
-
-f=rand()%10;
-
-//if f=5 frame is discarded for some reason
-
-//else frame is correctly recieved
-
-if(f!=5)
-
-{
-
-for(int j=0;j<fr_send_at_instance;j++)
-
-if(rcvd[j]==send[i])
-
-{
-
-cout<<"\nreciever:Frame "<<rcvd[j]<< "recieved correctly\n";
-
-rcvd[j]=arr[rw];
-
-rw=(rw+1)%m;
-
-break;
-
-}
-int j;
-if(j==fr_send_at_instance)
-
-cout<<"\nreciever:Duplicate frame "<<send[i]<<" discarded\n";
-
-a1=rand()%5;
-
-//if al==3 then ack is lost
-
-//else recieved
-
-if(a1==3)
-
-{
-
-cout<<"\n(acknowledgement "<<send[i]<<" lost)\n";
-
-cout<<"\n(sender timeouts-->Resend the frame)\n";
-
-rcvd_ack[i]='n';
-
-}
-
-else
-
-{
-
-cout<<"(acknowledgement "<<send[i]<<" recieved)\n";
-
-rcvd_ack[i]='p';
-
-}
-
-}
-
-else
-
-{int ld=rand()%2;
-
-//if =0 then frame damaged
-
-//else frame lost
-
-if(ld==0)
-
-{
-
-cout<<"\nRECEIVER : Frame "<<send[i]<<" is damaged\n";
-
-cout<<"\nRECEIVER : Negative Acknowledgement "<<send[i]<<" sent\n";
-
-}
-
-else
-
-{
-
-cout<<"\nRECEIVER : Frame "<<send[i]<<" is lost\n";
-
-cout<<"\n (SENDER TIMEOUTS-->RESEND THE FRAME)\n";
-
-}
-
-rcvd_ack[i]='n';
-
-}
-
-}
-
-}
-
-for(int j=0;j<fr_send_at_instance;j++)
-
-{
-
-if(rcvd_ack[j]=='n')
-break;
-
-}
-
-int i=0;
-
-for(int k=j;k<fr_send_at_instance;k++)
-{
-
-send[i]=send[k];
-if(rcvd_ack[k]=='n')
-  rcvd_ack[i]='n';
-else
-  rcvd_ack[i]='p';
-i++;
-
-}
-
-if(i!=fr_send_at_instance)
-{
- for(int k=i;k<fr_send_at_instance;k++)
   {
-   send[k]=arr[sw];
-   sw=(sw+1)%m;
-   rcvd_ack[k]='n';
+
+    if (rcvd_ack[i] == 'n')
+
+    {
+
+      f = rand() % 10;
+
+      // if f=5 frame is discarded for some reason
+
+      // else frame is correctly recieved
+
+      if (f != 5)
+
+      {
+
+        for (int j = 0; j < fr_send_at_instance; j++)
+
+          if (rcvd[j] == send[i])
+
+          {
+
+            cout << "\nreciever:Frame " << rcvd[j] << "recieved correctly\n";
+
+            rcvd[j] = arr[rw];
+
+            rw = (rw + 1) % m;
+
+            break;
+          }
+        int j;
+        if (j == fr_send_at_instance)
+
+          cout << "\nreciever:Duplicate frame " << send[i] << " discarded\n";
+
+        a1 = rand() % 5;
+
+        // if al==3 then ack is lost
+
+        // else recieved
+
+        if (a1 == 3)
+
+        {
+
+          cout << "\n(acknowledgement " << send[i] << " lost)\n";
+
+          cout << "\n(sender timeouts-->Resend the frame)\n";
+
+          rcvd_ack[i] = 'n';
+        }
+
+        else
+
+        {
+
+          cout << "(acknowledgement " << send[i] << " recieved)\n";
+
+          rcvd_ack[i] = 'p';
+        }
+      }
+
+      else
+
+      {
+        int ld = rand() % 2;
+
+        // if =0 then frame damaged
+
+        // else frame lost
+
+        if (ld == 0)
+
+        {
+
+          cout << "\nRECEIVER : Frame " << send[i] << " is damaged\n";
+
+          cout << "\nRECEIVER : Negative Acknowledgement " << send[i] << " sent\n";
+        }
+
+        else
+
+        {
+
+          cout << "\nRECEIVER : Frame " << send[i] << " is lost\n";
+
+          cout << "\n (SENDER TIMEOUTS-->RESEND THE FRAME)\n";
+        }
+
+        rcvd_ack[i] = 'n';
+      }
+    }
   }
-}
-cout<<"\nWant to continue('Y' or 'y' to continue): ";
-cin>>ch;
-cout<<"\n";
-if(ch=='y')
-   sender(m);
-else
-   exit(0);
+
+  for (int j = 0; j < fr_send_at_instance; j++)
+
+  {
+
+    if (rcvd_ack[j] == 'n')
+      break;
+  }
+
+  int i = 0;
+
+  for (int k = j; k < fr_send_at_instance; k++)
+  {
+
+    send[i] = send[k];
+    if (rcvd_ack[k] == 'n')
+      rcvd_ack[i] = 'n';
+    else
+      rcvd_ack[i] = 'p';
+    i++;
+  }
+
+  if (i != fr_send_at_instance)
+  {
+    for (int k = i; k < fr_send_at_instance; k++)
+    {
+      send[k] = arr[sw];
+      sw = (sw + 1) % m;
+      rcvd_ack[k] = 'n';
+    }
+  }
+  cout << "\nWant to continue('Y' or 'y' to continue): ";
+  cin >> ch;
+  cout << "\n";
+  if (ch == 'y')
+    sender(m);
+  else
+    exit(0);
 }
 
 int main()
 {
-  sel_repeat sr;;
+  sel_repeat sr;
+  ;
   sr.input();
 
   return 0;
